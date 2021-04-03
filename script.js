d7b8447832e9b67ccfca4b0398e7582a32467d27
jamiesmithjts <58477931+jamiesmithjts@users.noreply.github.com>
2021-04-03 13:32:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name?')
+const name = 'A random person'
 appendMessage('You joined')
-socket.emit('new-user', name)
+socket.emit('new-user')
 
 socket.on('chat-message', data =>{
     appendMessage(`${data.name}: ${data.message}`)
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const io = require('socket.io')(3000, {
     cors:{
         origin:"*",
